7d90aa15e5eea274472f5d8c43d5c5f2ba36c96b
bluefractalfish <blue@fractal.fish>
2026-02-19 11:10:04 -0700

    updates

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,7 +1,7 @@
 const header = document.getElementById("siteHeader");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 50){
+  if (window.scrollY > 5){
     header.classList.add("scrolled")
   } else {
     header.classList.remove(scrolled);
